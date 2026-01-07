d0bfffcf12314e12d6d2cafc35030a3cbfed0604
AnjanG7 <r65854453@gmail.com>
2026-01-07 15:29:34 +0545

    fix

--- apps/api/src/utils/helper.js
+++ apps/api/src/utils/helper.js
@@ -22,6 +22,9 @@ export const verifyToken = (token) => {
   try {
     return jwt.verify(token, JWT_SECRET);
   } catch (err) {
+     if (err.name === "TokenExpiredError") {
+      throw new Error("Token has expired");
+    }
     throw new Error("Invalid or expired token");
   }
 };
